613f5b1796dd67b48ffa95ea91b12f2ee2cf145e
freesftys <freesftys@gmail.com>
2014-01-04 22:29:14 +0000

    NJL7502L照度センサの読み取りの微調整

--- sensor/adConvert.h
+++ sensor/adConvert.h
@@ -2,11 +2,18 @@
 #ifndef ADCONVERT_H
 #define ADCONVERT_H
 
+//計算時のデータ
+#define AD_RESOLUTION	12		//ICの分解能
+#define AD_LSB			2		//ICの分解能で精度の問題で捨てるbit数
+#define VREF			3300	//基準ボルト(mV)
+
+
 int InitAD();
 int UnInitAD();
 
-int PinGetADCH(int pin, int ch, float *outADVolt, unsigned long sleepTime);
 
-int GetADCH(int CH, float *outADVolt);
+//捨てるlsbを設定
+int PinGetADCH(int pin, int ch, float *outADVolt, unsigned long sleepTime, unsigned int lsbBit);
+int GetADCH(int CH, float *outADVolt, unsigned int lsbBit);
 
 #endif
